202cebad3a862147ad12b492d0fd1d0730311530
QHuyyyyy <huy120904@gmail.com>
2025-10-01 18:24:36 +0700

    rm code

--- middleware.ts
+++ middleware.ts
@@ -46,9 +46,8 @@ export const config = {
          * - api (API routes)
          * - _next/static (static files)
          * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
          * - images (public images)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico|images).*)',
+        '/((?!api|_next/static|_next/image|images).*)',
     ],
 };
